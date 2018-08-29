c410bfef1508f7ab05d201ce6a0aa9ed2b0af4dc
Jasmine Walters <jsmnwltrs@gmail.com>
2018-08-28 19:08:49 -0500

    completed array challenge

--- arrays.js
+++ arrays.js
@@ -1,1 +1,31 @@
-console.log('hello')
+const cats = ['fluffy', 'spike', 'killer'];
+console.log('1st cat', cats[0]);
+
+const favoriteAnimal = 'cow';
+const favoriteAnimalArray = favoriteAnimal.split(''); //['c', 'o', 'w']
+console.log('favoriteAnimalArray', favoriteAnimalArray);
+
+const favoriteAnimals = 'cow,dog,duck,bear';
+const favoriteAnimalsArray = favoriteAnimals.split(','); //["cow", "dog", "duck", "bear"]
+console.log('favoriteAnimalsArray', favoriteAnimalsArray);
+
+const numArray = [1, 2, 3, 4, 5];
+const newNum = numArray.join(''); //12345
+console.log('newNum', newNum);
+
+//mini challenge
+
+
+
+const palChecker = (word) => {
+    const wordArray = word.split ('').join('');
+    const reverseArray = word.split('').reverse().join('');
+    if (wordArray === reverseArray) {
+    return console.log ('Yes it is a palindrome');
+} else {
+    console.log('No it is not a palindrome');
+}};
+
+palChecker('mom');
+palChecker('dumb');
+palChecker('racecar');
